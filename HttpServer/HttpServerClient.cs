82dc7919d9d98d29aa1fc1ea7444ca7fa83bd685
Kenya <kenni.kr@hotmail.com>
2024-05-17 15:01:35 +0200

    Add Stock Riven

--- HttpServer/HttpServerClient.cs
+++ HttpServer/HttpServerClient.cs
@@ -5,6 +5,7 @@ using QuantframeLib.Utils;
 using SimpleHTTP;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Text;
@@ -23,6 +24,13 @@ namespace QuantframeLib.HttpServer
         private static CancellationTokenSource _token;
         #endregion
         #region New
+        public static string GetItemType(string displayName)
+        {
+            string type = "";
+
+            return type;
+        }
+
         public static void Initializer(string hostname, int port)
         {
             _token = new CancellationTokenSource();
@@ -35,6 +43,20 @@ namespace QuantframeLib.HttpServer
             _server.Routes.Add("/", (rq, rp, args) =>
             {
                 rp.WithCORS().AsText("asd");
+            });
+            _server.Routes.Add("/test", (rq, rp, args) =>
+            {
+                string data = File.ReadAllText(@"C:\Users\Kenni Kristiansen\Downloads\Fivem\quantframe-api\backend\target\trades.json");
+                TypedProduct[] trades = Misc.Deserialize<TypedProduct[]>(data);
+                StringBuilder sb = new StringBuilder();
+                foreach (var trade in trades)
+                {
+                    foreach (var item in trade.Offerings)
+                        sb.AppendLine($"{item.DisplayName}: {GetItemType(item.DisplayName)}");
+                    foreach (var item in trade.Receiving)
+                        sb.AppendLine($"{item.DisplayName}: {GetItemType(item.DisplayName)}");
+                }
+                rp.WithCORS().AsText(sb.ToString());
             });
             _server.Routes.Add("/stock/add_riven_alecaframe", (rq, rp, args) =>
             {
@@ -53,9 +75,33 @@ namespace QuantframeLib.HttpServer
             }, "POST");
             _server.Routes.Add("/stock/add_riven", (rq, rp, args) =>
             {
-                var asd = rq.GetBodyAsString();
-                Logger.Info("HttpServerClient:AddRiven", asd);
-                rp.WithCORS().AsText("{}");
+                try
+                {
+                    string payload = rq.GetBodyAsString();
+                    AddStockRiven eventObj = Misc.Deserialize<AddStockRiven>(payload);
+                    //SocketServer.Send("OnAddStockRiven", eventObj.ToJson());
+                    rp.WithCORS().AsText("{}");
+                }
+                catch (Exception ex)
+                {
+                    Logger.Error("SocketServer:OnMessageReceived", "Invalid message received: " + ex.Message);
+                    rp.WithCORS().AsText(ex.Message);
+                }
+            }, "POST");
+            _server.Routes.Add("/stock/add_item", (rq, rp, args) =>
+            {
+                try
+                {
+                    string payload = rq.GetBodyAsString();
+                    AddStockItem eventObj = Misc.Deserialize<AddStockItem>(payload);
+                    //SocketServer.Send("OnAddItem", eventObj.ToJson());
+                    rp.WithCORS().AsText("{}");
+                }
+                catch (Exception ex)
+                {
+                    Logger.Error("SocketServer:OnMessageReceived", "Invalid message received: " + ex.Message);
+                    rp.WithCORS().AsText(ex.Message);
+                }
             }, "POST");
             _server.Start();
         }
